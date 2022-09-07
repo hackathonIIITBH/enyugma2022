e0041a7768616a5d4c56b7c962655ad47095a6d3
Shreyash Kumar <kumarshreyash39@gmail.com>
2022-09-07 15:40:12 +0530

    added timer with responsive

---
+++ assets/js/timer.js
@@ -0,0 +1,24 @@
+var countToDate = new Date("Oct 16, 2022 18:37:25").getTime();
+
+var x = setInterval(function(){
+
+    var now = new Date().getTime();
+
+    var distance = countToDate-now;
+
+    var days = Math.floor(distance/(1000*60*60*24));
+    var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+    var seconds = Math.floor((distance%(1000*60))/1000);
+
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "COMING SOON";
+    }
+},1000) 
