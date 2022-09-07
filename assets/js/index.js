18789f0c65da746b1350aa30052909823471b072
shrey2003 <shreymishra0307@gmail.com>
2022-09-07 19:57:42 +0530

    Register button and background change

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,73 +4,73 @@
 
 var renderer = new THREE.WebGLRenderer({
     antialias:true
-});
-
-/*FullScreen */
-
-renderer.setSize(window.innerWidth,window.innerHeight);
-
-
-/*Append to HTML */
-document.body.appendChild(renderer.domElement);
-var onRenderFcts = [];
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight, 0.01,1000);
-
-/*Camera Positioning*/
-
-camera.position.z=15;
-camera.position.y=2;
-
-/* Fog Provides depth to the landscape*/
-
-scene.fog = new THREE.Fog(0x000, 0, 45);
-;(function(){
+  });
+  
+  /*FullScreen */
+  
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  
+  
+  /*Append to HTML */
+  document.body.appendChild(renderer.domElement);
+  var onRenderFcts = [];
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight, 0.01,1000);
+  
+  /*Camera Positioning*/
+  
+  camera.position.z=15;
+  camera.position.y=2;
+  
+  /* Fog Provides depth to the landscape*/
+  
+  scene.fog = new THREE.Fog(0x000, 0, 45);
+  ;(function(){
     var light = new THREE.AmbientLight(0x202020)
     scene.add(light)
     var light = new THREE.DirectionalLight('white',5);
     light.position.set(0.5,0.0,2);
     scene.add(light);
     var light	= new THREE.DirectionalLight('white', 0.75*2);
-	light.position.set(-0.5, -0.5, -2);
-	scene.add( light );
-})()
- 
-var heightMap = THREEx.Terrain.allocateHeightMap(256,256)
-THREEx.Terrain.simplexHeightMap(heightMap);
-
-var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap);
-THREEx.Terrain.heightMapToVertexColor(heightMap,geometry);
-
-/*Wireframe*/
-
-var material = new THREE.MeshBasicMaterial({
+  light.position.set(-0.5, -0.5, -2);
+  scene.add( light );
+  })()
+  
+  var heightMap = THREEx.Terrain.allocateHeightMap(256,256)
+  THREEx.Terrain.simplexHeightMap(heightMap);
+  
+  var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap);
+  THREEx.Terrain.heightMapToVertexColor(heightMap,geometry);
+  
+  /*Wireframe*/
+  
+  var material = new THREE.MeshBasicMaterial({
     wireframe:true
-});
-
-var mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-mesh.lookAt(new THREE.Vector3(0,1,0));
-
-mesh.scale.y	= 3.5;
-mesh.scale.x	= 3;
-mesh.scale.z	= 0.20;
-mesh.scale.multiplyScalar(10);
-
-
-onRenderFcts.push(function(delta, now){
+  });
+  
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  mesh.lookAt(new THREE.Vector3(0,1,0));
+  
+  mesh.scale.y	= 3.5;
+  mesh.scale.x	= 3;
+  mesh.scale.z	= 0.20;
+  mesh.scale.multiplyScalar(10);
+  
+  
+  onRenderFcts.push(function(delta, now){
     mesh.rotation.z += 0.03 * delta;	
-})
-onRenderFcts.push(function(){
+  })
+  onRenderFcts.push(function(){
     renderer.render( scene, camera );		
-})
-
-
-// onRenderFcts.push(function(){
-//     renderer.render( scene, camera );		
-// })
-var lastTimeMsec= null
-requestAnimationFrame(function animate(nowMsec){
+  })
+  
+  
+  // onRenderFcts.push(function(){
+  //     renderer.render( scene, camera );		
+  // })
+  var lastTimeMsec= null
+  requestAnimationFrame(function animate(nowMsec){
     requestAnimationFrame( animate );
     lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
     var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
@@ -78,4 +78,5 @@ requestAnimationFrame(function animate(nowMsec){
     onRenderFcts.forEach(function(onRenderFct){
         onRenderFct(deltaMsec/1000, nowMsec/1000)
     })
-})
+  })
+  
