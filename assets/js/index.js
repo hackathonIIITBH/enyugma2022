ea9ca42a26cb972c2c969e0fe66ccf5f939c8e3c
Shreyash Kumar <kumarshreyash39@gmail.com>
2022-09-09 00:52:40 +0530

    responsive view changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,32 @@
+var container ;
+var camera,controls,scene,renderer;
+var w,h;
 
-// document.write("<h2>Hello World!</h2><p>Have a nice day!</p>");
-// document.write("<h2>Hello World!</h2><p>Have a nice day!</p>");
-
-var renderer = new THREE.WebGLRenderer({
+init();
+render();
+function init(){
+  container = document.getElementById('canvas');
+  renderer = new THREE.WebGLRenderer({
     antialias:true
   });
-  
-  /*FullScreen */
-  
-  renderer.setSize(window.innerWidth,window.innerHeight);
-  
-  
+  w = container.offsetWidth;
+  h = container.offsetHeight;
+
+  /*conatainer Screen */
+
+  renderer.setSize(w,h);
+
   /*Append to HTML */
-  document.body.appendChild(renderer.domElement);
+  container.appendChild(renderer.domElement);
   var onRenderFcts = [];
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight, 0.01,1000);
-  
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(25, w/h, 0.01,1000);
+
   /*Camera Positioning*/
   
   camera.position.z=15;
   camera.position.y=2;
-  
+
   /* Fog Provides depth to the landscape*/
   
   scene.fog = new THREE.Fog(0x000, 0, 45);
@@ -41,7 +46,7 @@ var renderer = new THREE.WebGLRenderer({
   
   var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap);
   THREEx.Terrain.heightMapToVertexColor(heightMap,geometry);
-  
+
   /*Wireframe*/
   
   var material = new THREE.MeshBasicMaterial({
@@ -64,11 +69,7 @@ var renderer = new THREE.WebGLRenderer({
   onRenderFcts.push(function(){
     renderer.render( scene, camera );		
   })
-  
-  
-  // onRenderFcts.push(function(){
-  //     renderer.render( scene, camera );		
-  // })
+
   var lastTimeMsec= null
   requestAnimationFrame(function animate(nowMsec){
     requestAnimationFrame( animate );
@@ -79,4 +80,73 @@ var renderer = new THREE.WebGLRenderer({
         onRenderFct(deltaMsec/1000, nowMsec/1000)
     })
   })
+}
+
+
+  // var onRenderFcts = [];
+  // var scene = new THREE.Scene();
+  // var camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight, 0.01,1000);
+  
+  /*Camera Positioning*/
+  
+  // camera.position.z=15;
+  // camera.position.y=2;
+  
+  /* Fog Provides depth to the landscape*/
+  
+  // scene.fog = new THREE.Fog(0x000, 0, 45);
+  // ;(function(){
+  //   var light = new THREE.AmbientLight(0x202020)
+  //   scene.add(light)
+  //   var light = new THREE.DirectionalLight('white',5);
+  //   light.position.set(0.5,0.0,2);
+  //   scene.add(light);
+  //   var light	= new THREE.DirectionalLight('white', 0.75*2);
+  // light.position.set(-0.5, -0.5, -2);
+  // scene.add( light );
+  // })()
+  
+  // var heightMap = THREEx.Terrain.allocateHeightMap(256,256)
+  // THREEx.Terrain.simplexHeightMap(heightMap);
+  
+  // var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap);
+  // THREEx.Terrain.heightMapToVertexColor(heightMap,geometry);
+  
+  /*Wireframe*/
+  
+  // var material = new THREE.MeshBasicMaterial({
+  //   wireframe:true
+  // });
+  
+  // var mesh = new THREE.Mesh(geometry, material);
+  // scene.add(mesh);
+  // mesh.lookAt(new THREE.Vector3(0,1,0));
+  
+  // mesh.scale.y	= 3.5;
+  // mesh.scale.x	= 3;
+  // mesh.scale.z	= 0.20;
+  // mesh.scale.multiplyScalar(10);
+  
+  
+  // onRenderFcts.push(function(delta, now){
+  //   mesh.rotation.z += 0.03 * delta;	
+  // })
+  // onRenderFcts.push(function(){
+  //   renderer.render( scene, camera );		
+  // })
+  
+  
+  // onRenderFcts.push(function(){
+  //     renderer.render( scene, camera );		
+  // })
+  // var lastTimeMsec= null
+  // requestAnimationFrame(function animate(nowMsec){
+  //   requestAnimationFrame( animate );
+  //   lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
+  //   var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
+  //   lastTimeMsec	= nowMsec
+  //   onRenderFcts.forEach(function(onRenderFct){
+  //       onRenderFct(deltaMsec/1000, nowMsec/1000)
+  //   })
+  // })
   
