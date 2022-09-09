2db09062c4f65a38c6b9299e64c8de7ff6175bc6
prateekch33 <prateekchandra27@outlook.com>
2022-09-09 14:36:10 +0530

    Loader Attached

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,152 +1,151 @@
-var container ;
-var camera,controls,scene,renderer;
-var w,h;
+var container;
+var camera, controls, scene, renderer;
+var w, h;
 
+setTimeout(() => {
+  document.querySelector(".loader").style.display = "none";
+  document.querySelector(".landing-page").style.display = "block";
+}, 7000);
 init();
 render();
-function init(){
-  container = document.getElementById('canvas');
+function init() {
+  container = document.getElementById("canvas");
   renderer = new THREE.WebGLRenderer({
-    antialias:true
+    antialias: true,
   });
   w = container.offsetWidth;
   h = container.offsetHeight;
 
   /*conatainer Screen */
 
-  renderer.setSize(w,h);
+  renderer.setSize(w, h);
 
   /*Append to HTML */
   container.appendChild(renderer.domElement);
   var onRenderFcts = [];
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(25, w/h, 0.01,1000);
+  camera = new THREE.PerspectiveCamera(25, w / h, 0.01, 1000);
 
   /*Camera Positioning*/
-  
-  camera.position.z=15;
-  camera.position.y=2;
+
+  camera.position.z = 15;
+  camera.position.y = 2;
 
   /* Fog Provides depth to the landscape*/
-  
+
   scene.fog = new THREE.Fog(0x000, 0, 45);
-  ;(function(){
-    var light = new THREE.AmbientLight(0x202020)
-    scene.add(light)
-    var light = new THREE.DirectionalLight('white',5);
-    light.position.set(0.5,0.0,2);
+  (function () {
+    var light = new THREE.AmbientLight(0x202020);
     scene.add(light);
-    var light	= new THREE.DirectionalLight('white', 0.75*2);
-  light.position.set(-0.5, -0.5, -2);
-  scene.add( light );
-  })()
-  
-  var heightMap = THREEx.Terrain.allocateHeightMap(256,256)
+    var light = new THREE.DirectionalLight("white", 5);
+    light.position.set(0.5, 0.0, 2);
+    scene.add(light);
+    var light = new THREE.DirectionalLight("white", 0.75 * 2);
+    light.position.set(-0.5, -0.5, -2);
+    scene.add(light);
+  })();
+
+  var heightMap = THREEx.Terrain.allocateHeightMap(256, 256);
   THREEx.Terrain.simplexHeightMap(heightMap);
-  
+
   var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap);
-  THREEx.Terrain.heightMapToVertexColor(heightMap,geometry);
+  THREEx.Terrain.heightMapToVertexColor(heightMap, geometry);
 
   /*Wireframe*/
-  
+
   var material = new THREE.MeshBasicMaterial({
-    wireframe:true
+    wireframe: true,
   });
-  
+
   var mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
-  mesh.lookAt(new THREE.Vector3(0,1,0));
-  
-  mesh.scale.y	= 3.5;
-  mesh.scale.x	= 3;
-  mesh.scale.z	= 0.20;
-  mesh.scale.multiplyScalar(10);
-  
-  
-  onRenderFcts.push(function(delta, now){
-    mesh.rotation.z += 0.03 * delta;	
-  })
-  onRenderFcts.push(function(){
-    renderer.render( scene, camera );		
-  })
+  mesh.lookAt(new THREE.Vector3(0, 1, 0));
 
-  var lastTimeMsec= null
-  requestAnimationFrame(function animate(nowMsec){
-    requestAnimationFrame( animate );
-    lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
-    var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
-    lastTimeMsec	= nowMsec
-    onRenderFcts.forEach(function(onRenderFct){
-        onRenderFct(deltaMsec/1000, nowMsec/1000)
-    })
-  })
+  mesh.scale.y = 3.5;
+  mesh.scale.x = 3;
+  mesh.scale.z = 0.2;
+  mesh.scale.multiplyScalar(10);
+
+  onRenderFcts.push(function (delta, now) {
+    mesh.rotation.z += 0.03 * delta;
+  });
+  onRenderFcts.push(function () {
+    renderer.render(scene, camera);
+  });
+
+  var lastTimeMsec = null;
+  requestAnimationFrame(function animate(nowMsec) {
+    requestAnimationFrame(animate);
+    lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60;
+    var deltaMsec = Math.min(200, nowMsec - lastTimeMsec);
+    lastTimeMsec = nowMsec;
+    onRenderFcts.forEach(function (onRenderFct) {
+      onRenderFct(deltaMsec / 1000, nowMsec / 1000);
+    });
+  });
 }
 
+// var onRenderFcts = [];
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight, 0.01,1000);
 
-  // var onRenderFcts = [];
-  // var scene = new THREE.Scene();
-  // var camera = new THREE.PerspectiveCamera(25, window.innerWidth/window.innerHeight, 0.01,1000);
-  
-  /*Camera Positioning*/
-  
-  // camera.position.z=15;
-  // camera.position.y=2;
-  
-  /* Fog Provides depth to the landscape*/
-  
-  // scene.fog = new THREE.Fog(0x000, 0, 45);
-  // ;(function(){
-  //   var light = new THREE.AmbientLight(0x202020)
-  //   scene.add(light)
-  //   var light = new THREE.DirectionalLight('white',5);
-  //   light.position.set(0.5,0.0,2);
-  //   scene.add(light);
-  //   var light	= new THREE.DirectionalLight('white', 0.75*2);
-  // light.position.set(-0.5, -0.5, -2);
-  // scene.add( light );
-  // })()
-  
-  // var heightMap = THREEx.Terrain.allocateHeightMap(256,256)
-  // THREEx.Terrain.simplexHeightMap(heightMap);
-  
-  // var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap);
-  // THREEx.Terrain.heightMapToVertexColor(heightMap,geometry);
-  
-  /*Wireframe*/
-  
-  // var material = new THREE.MeshBasicMaterial({
-  //   wireframe:true
-  // });
-  
-  // var mesh = new THREE.Mesh(geometry, material);
-  // scene.add(mesh);
-  // mesh.lookAt(new THREE.Vector3(0,1,0));
-  
-  // mesh.scale.y	= 3.5;
-  // mesh.scale.x	= 3;
-  // mesh.scale.z	= 0.20;
-  // mesh.scale.multiplyScalar(10);
-  
-  
-  // onRenderFcts.push(function(delta, now){
-  //   mesh.rotation.z += 0.03 * delta;	
-  // })
-  // onRenderFcts.push(function(){
-  //   renderer.render( scene, camera );		
-  // })
-  
-  
-  // onRenderFcts.push(function(){
-  //     renderer.render( scene, camera );		
-  // })
-  // var lastTimeMsec= null
-  // requestAnimationFrame(function animate(nowMsec){
-  //   requestAnimationFrame( animate );
-  //   lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
-  //   var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
-  //   lastTimeMsec	= nowMsec
-  //   onRenderFcts.forEach(function(onRenderFct){
-  //       onRenderFct(deltaMsec/1000, nowMsec/1000)
-  //   })
-  // })
-  
+/*Camera Positioning*/
+
+// camera.position.z=15;
+// camera.position.y=2;
+
+/* Fog Provides depth to the landscape*/
+
+// scene.fog = new THREE.Fog(0x000, 0, 45);
+// ;(function(){
+//   var light = new THREE.AmbientLight(0x202020)
+//   scene.add(light)
+//   var light = new THREE.DirectionalLight('white',5);
+//   light.position.set(0.5,0.0,2);
+//   scene.add(light);
+//   var light	= new THREE.DirectionalLight('white', 0.75*2);
+// light.position.set(-0.5, -0.5, -2);
+// scene.add( light );
+// })()
+
+// var heightMap = THREEx.Terrain.allocateHeightMap(256,256)
+// THREEx.Terrain.simplexHeightMap(heightMap);
+
+// var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap);
+// THREEx.Terrain.heightMapToVertexColor(heightMap,geometry);
+
+/*Wireframe*/
+
+// var material = new THREE.MeshBasicMaterial({
+//   wireframe:true
+// });
+
+// var mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+// mesh.lookAt(new THREE.Vector3(0,1,0));
+
+// mesh.scale.y	= 3.5;
+// mesh.scale.x	= 3;
+// mesh.scale.z	= 0.20;
+// mesh.scale.multiplyScalar(10);
+
+// onRenderFcts.push(function(delta, now){
+//   mesh.rotation.z += 0.03 * delta;
+// })
+// onRenderFcts.push(function(){
+//   renderer.render( scene, camera );
+// })
+
+// onRenderFcts.push(function(){
+//     renderer.render( scene, camera );
+// })
+// var lastTimeMsec= null
+// requestAnimationFrame(function animate(nowMsec){
+//   requestAnimationFrame( animate );
+//   lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
+//   var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
+//   lastTimeMsec	= nowMsec
+//   onRenderFcts.forEach(function(onRenderFct){
+//       onRenderFct(deltaMsec/1000, nowMsec/1000)
+//   })
+// })
--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,119 +1,136 @@
-function init(){
+function init() {
+  //estrelas
 
-    //estrelas
-  
-    var style = ["style1", "style2", "style3", "style4"];
-    var tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
-    var opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
-  
-    function getRandomArbitrary(min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
-  
-    var estrela = "";
-    var qtdeEstrelas = 250;
-    var noite = document.querySelector(".constelacao");
-    var widthWindow = window.innerWidth;
-    var heightWindow = window.innerHeight;
-  
-    for (var i = 0; i < qtdeEstrelas; i++) {
-      estrela += "<span class='estrela " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
-      + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." +getRandomArbitrary(0, 9)+ "s; left: "
-      + getRandomArbitrary(0, widthWindow) + "px; top: " + getRandomArbitrary(0, heightWindow) + "px;'></span>";
-    }
-  
-    noite.innerHTML = estrela;
-    //meteoros
-  
-    var numeroAleatorio = 5000;
-    setTimeout(function(){
-      carregarMeteoro();
-    }, numeroAleatorio);
-  
-    function carregarMeteoro(){
-      setTimeout(carregarMeteoro, numeroAleatorio);
-      numeroAleatorio = getRandomArbitrary(5000, 10000);
-      var meteoro = "<div class='meteoro "+ style[getRandomArbitrary(0, 4)] +"'></div>";
-      document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = meteoro;
-      setTimeout(function(){
-        document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = "";
-      }, 1000);
-    }
-  
+  var style = ["style1", "style2", "style3", "style4"];
+  var tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
+  var opacity = [
+    "opacity1",
+    "opacity1",
+    "opacity1",
+    "opacity2",
+    "opacity2",
+    "opacity3",
+  ];
+
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
-  
-  window.onload = init;
 
+  var estrela = "";
+  var qtdeEstrelas = 250;
+  var noite = document.querySelector(".constelacao");
+  var widthWindow = window.innerWidth;
+  var heightWindow = window.innerHeight;
 
+  for (var i = 0; i < qtdeEstrelas; i++) {
+    estrela +=
+      "<span class='estrela " +
+      style[getRandomArbitrary(0, 4)] +
+      " " +
+      opacity[getRandomArbitrary(0, 6)] +
+      " " +
+      tam[getRandomArbitrary(0, 5)] +
+      "' style='animation-delay: ." +
+      getRandomArbitrary(0, 9) +
+      "s; left: " +
+      getRandomArbitrary(0, widthWindow) +
+      "px; top: " +
+      getRandomArbitrary(0, heightWindow) +
+      "px;'></span>";
+  }
 
+  noite.innerHTML = estrela;
+  //meteoros
 
-  
- var letterframes = [
-	{ 
-		transform: 'translateY(80px)',
-		opacity: 0
-	},
-	{ 
-		transform: 'translateY(-20px)', 
-		opacity: 1,
-		offset: 0.01
-	},
-	{ 
-		transform: 'translateY(0)', 
-		offset: 0.02,
-	},
-	{ 
-		transform: 'translateY(0) scale(1)', 
-		opacity: 1,
-		offset: 0.9
-	},
-	{ 
-		transform: 'translateY(300px) rotateZ(120deg)', 
-		opacity: 0
-	}
-]
+  var numeroAleatorio = 5000;
+  setTimeout(function () {
+    carregarMeteoro();
+  }, numeroAleatorio);
 
-function wrapInSpan(text){ // split each character of text and wrap inside span tag
-	return text.split('').map(letter => {
-		var letter = (letter == ' ')? '&nbsp;' : letter
-		return '<span>' + letter + '</span>'
-	})
-	.join('')
+  function carregarMeteoro() {
+    setTimeout(carregarMeteoro, numeroAleatorio);
+    numeroAleatorio = getRandomArbitrary(5000, 10000);
+    var meteoro =
+      "<div class='meteoro " + style[getRandomArbitrary(0, 4)] + "'></div>";
+    document.getElementsByClassName("chuvaMeteoro")[0].innerHTML = meteoro;
+    setTimeout(function () {
+      document.getElementsByClassName("chuvaMeteoro")[0].innerHTML = "";
+    }, 1000);
+  }
+}
+init();
+
+var letterframes = [
+  {
+    transform: "translateY(80px)",
+    opacity: 0,
+  },
+  {
+    transform: "translateY(-20px)",
+    opacity: 1,
+    offset: 0.01,
+  },
+  {
+    transform: "translateY(0)",
+    offset: 0.02,
+  },
+  {
+    transform: "translateY(0) scale(1)",
+    opacity: 1,
+    offset: 0.9,
+  },
+  {
+    transform: "translateY(300px) rotateZ(120deg)",
+    opacity: 0,
+  },
+];
+
+function wrapInSpan(text) {
+  // split each character of text and wrap inside span tag
+  return text
+    .split("")
+    .map((letter) => {
+      var letter = letter == " " ? "&nbsp;" : letter;
+      return "<span>" + letter + "</span>";
+    })
+    .join("");
 }
 
-var header = document.getElementById('header')
-var letterAnimations = [] // array of letters animation objects
-var lettersArray // array of letters DOM references
+var header = document.getElementById("header");
+var letterAnimations = []; // array of letters animation objects
+var lettersArray; // array of letters DOM references
 
-header.innerHTML = (wrapInSpan(header.innerText))
-lettersArray = Array.from(header.getElementsByTagName('span'))
+header.innerHTML = wrapInSpan(header.innerText);
+lettersArray = Array.from(header.getElementsByTagName("span"));
 
-lettersArray.map((letter, indx) =>{ // loop through lettersArray
-	letterAnimations[indx] = letter.animate(letterframes, { // Call animate() on each letter and store reference to Animation() object instance
-		fill: 'both',
-		easing: 'ease-in',
-		duration: 4000,
-		delay: indx * 300
-	})
-})
+lettersArray.map((letter, indx) => {
+  // loop through lettersArray
+  letterAnimations[indx] = letter.animate(letterframes, {
+    // Call animate() on each letter and store reference to Animation() object instance
+    fill: "both",
+    easing: "ease-in",
+    duration: 4000,
+    delay: indx * 300,
+  });
+});
 
-
-function playLetters(){
-	 return letterAnimations.map((animation) =>{ // call each animation instance play() method and return array of animation promises
-		animation.play()
-		return animation.finished // return promise that resolve when animation finishes
-	})
+function playLetters() {
+  return letterAnimations.map((animation) => {
+    // call each animation instance play() method and return array of animation promises
+    animation.play();
+    return animation.finished; // return promise that resolve when animation finishes
+  });
 }
 
-
-function pauseletters(){
-	letterAnimations.map((animation) =>{
-		animation.cancel()
-	})
+function pauseletters() {
+  letterAnimations.map((animation) => {
+    animation.cancel();
+  });
 }
 
-pauseletters() // pause animation to start
+pauseletters(); // pause animation to start
 
-Promise.all(playLetters()).then(() => { // play animation sequence and do something when they have all completed
-	pauseletters() // cancel animation (so text appears again)
-})
+Promise.all(playLetters()).then(() => {
+  // play animation sequence and do something when they have all completed
+  pauseletters(); // cancel animation (so text appears again)
+});
