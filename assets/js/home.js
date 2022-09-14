ee5701819d3d071218076a61f0b238caa0834726
Dhruv-2134 <dhruvpersonal1@gmail.com>
2022-09-14 16:36:50 +0530

    added navbar

---
+++ assets/js/home.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+    $(".setbtn").css("display","none");
+    setTimeout(() => {
+        $(".setbtn").css("display","");
+    },17050);
+})
+  
+  function sets() {
+      $(".settingbar").css("--settingbar-display", "flex");
+      $(".body").css("transform-origin", "left");
+      $(".settingbar").css("transform", "rotateY(0) translateX(-25vw)");
+      window.scrollTo(0, 0);
+      $("body").css("overflow", "hidden");
+      $(".displaybox").css("height", "50%");
+    return false;
+  }
+  
+  function closesettingbar() {
+      $(".settingbar").css("--settingbar-display", "none");
+      $(".body").css("transform-origin", "");
+      $(".body").css("transform", "");
+      $(".settingbar").css("transform", "rotateY(90deg)");
+      $("body").css("overflow", "");
+      $(".displaybox").css("height", "");
+
+  }
+  
