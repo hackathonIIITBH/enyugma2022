641f8b54c1e22d677ae29c75375ba8af1ab2cbf9
Ayaka <user.satoshi69@gmail.com>
2022-10-10 17:53:51 +0530

    new navbar prototype

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,31 +1,11 @@
-$(document).ready(() => {
-    $(".setbtn").css("display", "none");
-    setTimeout(() => {
-        $(".setbtn").css("display", "");
-    }, 17050);
-});
-
-function sets() {
-    $(".settingbar").css("--settingbar-display", "flex");
-    setTimeout(() => {
-        $(".body").css("transform-origin", "left");
-        $(".settingbar").css("transform", "rotateY(0) translateX(-25vw)");
-        window.scrollTo(0, 0);
-        $("body").css("overflow", "hidden");
-        $(".displaybox").css("height", "50%");
+$(document).ready(function () {
+    $(".navbar").scroll(function () {
+        //hide navbar when scroll down
+        
+        if ($(this).scrollTop() > 50) {
+            $(".navbar").css("opacity", "0");
+        } else {
+            $(".navbar").css("opacity", "1");
+        }
     });
-
-    return false;
-}
-
-function closesettingbar() {
-    $(".body").css("transform-origin", "");
-    $(".body").css("transform", "");
-    $(".settingbar").css("transform", "rotateY(90deg)");
-    $("body").css("overflow", "");
-    $( ".displaybox" ).css( "height", "" );
-        setTimeout(() => {
-            $(".settingbar").css("--settingbar-display", "none");
-        },500);
-
-}
+});
